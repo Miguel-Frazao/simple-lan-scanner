b7dd8d1e1db12b3bb7fd6b5cae8b6215e3c2f4cd
Miguel Frazão <miguelmssfrazao@gmail.com>
2016-11-25 14:58:57 +0000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,9 +1,9 @@
 import socket
 
+host = socket.gethostbyname(socket.gethostname())
+param = host.split('.')[-1] # getting the octet before the last one
 for i in range(255):
 	try:
-		host = socket.gethostbyname(socket.gethostname())
-		param = host.split('.')[-1] # getting the octet before the last one
 		found = socket.gethostbyaddr('192.168.{}.{}'.format(param, i))
 	except Exception as err:
 		continue
