12566b0bdff8c036ba09d00d3feddb364fe91b3e
Miguel Frazão <miguelmssfrazao@gmail.com>
2016-11-25 09:46:19 +0000

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,10 @@
+import socket
+
+for i in range(255):
+	try:
+		host = socket.gethostbyname(socket.gethostname())
+		param = host.split('.')[-1] # checking the octet before the last one
+		found = socket.gethostbyaddr('192.168.{}.{}'.format(param, i))
+	except Exception as err:
+		continue
+	print('[+] {}'.format(found))
