2c169c6ec9e5fb38b6fa1df4cbdcbb7b5d4849d2
Miguel Frazão <miguelmssfrazao@gmail.com>
2016-12-13 10:03:11 +0000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -5,7 +5,7 @@ s.connect(("gmail.com",80))
 host = s.getsockname()[0] # getting network ip
 s.close()
 
-octets = host.split('.') # getting the octet before the last one
+octets = host.split('.') # getting the octets
 for i in range(255):
 	try:
 		found = socket.gethostbyaddr('{}.{}.{}.{}'.format(octets[0], octets[1], octets[2], i))
