1a1399581d200f4249e8b9cb75b39fa287a109f6
Miguel Frazão <miguelmssfrazao@gmail.com>
2016-12-13 10:41:24 +0000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -2,10 +2,10 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("gmail.com",80))
-host = s.getsockname()[0] # getting network ip
+lan_ip, sock_port = s.getsockname() # getting network ip and port... not using port though
 s.close()
 
-octets = host.split('.') # getting the octets
+octets = lan_ip.split('.') # getting the octets
 for i in range(255):
 	try:
 		found = socket.gethostbyaddr('{}.{}.{}.{}'.format(octets[0], octets[1], octets[2], i))
