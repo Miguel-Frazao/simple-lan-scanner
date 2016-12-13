e8fb1e432a87a13c0ec60cec8ef6ba1250152bc8
Miguel Frazão <miguelmssfrazao@gmail.com>
2016-12-13 10:02:20 +0000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -2,13 +2,13 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("gmail.com",80))
-host = s.getsockname()[0]
+host = s.getsockname()[0] # getting network ip
 s.close()
 
-param = host.split('.')[-2] # getting the octet before the last one
+octets = host.split('.') # getting the octet before the last one
 for i in range(255):
 	try:
-		found = socket.gethostbyaddr('192.168.{}.{}'.format(param, i))
+		found = socket.gethostbyaddr('{}.{}.{}.{}'.format(octets[0], octets[1], octets[2], i))
 	except Exception as err:
 		continue
 	print('[+] {}'.format(found))
