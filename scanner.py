cb5dc463de1fa67375221be5fd164632b01d355a
Miguel Frazão <miguelmssfrazao@gmail.com>
2016-12-13 09:57:05 +0000

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,6 +1,6 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("gmail.com",80))
 host = s.getsockname()[0]
 s.close()
